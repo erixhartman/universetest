5271cb152f55c100c7bdf215f9da2c3652d455b9
Eric Hartman <imerichartman@gmail.com>
2017-07-26 11:38:20 -0400

    Make a dummy test to show how rspec works

--- spec/universe_test_spec.rb
+++ spec/universe_test_spec.rb
@@ -1,2 +1,14 @@
 require_relative 'spec_helper'
 require_relative '../universe'
+
+describe 'DummyTest' do
+
+  describe ".add" do
+    context 'two numbers' do
+      context "given '2,4'"
+        it 'returns 6' do
+          expect(DummyTest.add("2,4")).to eql(6)
+        end
+      end
+  end
+end
